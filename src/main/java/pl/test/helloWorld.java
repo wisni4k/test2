801e467cdfbd505c0e28449de0016b438d9b1895
Mateusz Wiśniewski <mateusz.wisniewski0@gmail.com>
2015-11-24 21:08:23 +0100

    first

---
+++ src/main/java/pl/test/helloWorld.java
@@ -0,0 +1,15 @@
+package pl.test;
+
+public class helloWorld {
+	
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
+
+}
